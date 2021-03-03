c3dc9c759e8e17ebbd0ce1a2788d962734148111
Tobias Ilg <tobias.ilg@capgemini.com>
2021-03-03 10:33:12 +0100

    Use user input to run commands

--- src/main/java/cmd/SimpleCmd.java
+++ src/main/java/cmd/SimpleCmd.java
@@ -1,58 +1,58 @@
 package cmd;
 
 import cmd.commands.BaseCommand;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Starting point of the Simple CMD. Contains the main method.
  */
 public class SimpleCmd {
 
-    /**
-     * Logger to output useful information to the user
-     */
-    final private static Logger LOG = LoggerFactory.getLogger(SimpleCmd.class);
+  /**
+   * Logger to output useful information to the user
+   */
+  private static final Logger LOG = LoggerFactory.getLogger(SimpleCmd.class);
 
-    /**
-     * register the {@link BaseCommand} in the new {@link CommandLine} Object
-     */
-    public static CommandLine commandLine = new CommandLine(new BaseCommand());
+  /**
+   * register the {@link BaseCommand} in the new {@link CommandLine} Object
+   */
+  public static final CommandLine commandLine = new CommandLine(new BaseCommand());
 
-    /**
-     * Main starting point of the CLI. Here we parse the arguments from the user.
-     *
-     * @param args list of arguments the user has passed
-     */
-    public static void main(String... args) {
-        Scanner scanner = new Scanner(System.in);
-        try {
-            while (true) {
-                System.out.println("Please input a line");
-                long then = System.currentTimeMillis();
-                String line = scanner.nextLine();
-                long now = System.currentTimeMillis();
-                System.out.printf("Waited %.3fs for user input%n", (now - then) / 1000d);
-                System.out.printf("User input was: %s%n", line);
-            }
-        } catch (IllegalStateException | NoSuchElementException e) {
-            // System.in has been closed
-            System.out.println("System.in was closed; exiting");
+  /**
+   * Main starting point of the CLI. Here we parse the arguments from the user.
+   *
+   * @param args list of arguments the user has passed
+   */
+  public static void main(String... args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      while (true) {
+        System.out.println("Please input a line");
+        long then = System.currentTimeMillis();
+        String line = scanner.nextLine();
+        long now = System.currentTimeMillis();
+        System.out.printf("Waited %.3fs for user input%n", (now - then) / 1000d);
+        System.out.printf("User input was: %s%n", line);
+
+        if (line.equals("exit")) {
+          break;
         }
 
-        boolean verbose = Arrays.asList(args).contains("-v");
+        String[] arguments = line.split(" ");
+
         LOG.debug("Current working directory: {}", System.getProperty("user.dir"));
 
-        if (commandLine.execute(args) == 0) {
-            LOG.debug("Commands were executed correctly");
+        if (commandLine.execute(arguments) == 0) {
+          LOG.debug("Commands were executed correctly");
         }
-
-
+      }
+    } catch (IllegalStateException | NoSuchElementException e) {
+      // System.in has been closed
+      System.out.println("System.in was closed; exiting");
     }
-
-}
+  }
