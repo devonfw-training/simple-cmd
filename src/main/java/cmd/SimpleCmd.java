cf2355e4d572991d1b26dd0aa0d1828b00dbc8eb
jbutschn <johann.butschnew@capgemini.com>
2021-03-12 15:36:43 +0100

    Added Javadoc comment

--- src/main/java/cmd/SimpleCmd.java
+++ src/main/java/cmd/SimpleCmd.java
@@ -14,56 +14,67 @@ import java.util.Scanner;
  */
 public class SimpleCmd {
 
-    /**
-     * register the {@link BaseCommand} in the new {@link CommandLine} Object
-     */
-    public static final CommandLine commandLine = new CommandLine(new BaseCommand());
-    /**
-     * Logger to output useful information to the user
-     */
-    private static final Logger LOG = LoggerFactory.getLogger(SimpleCmd.class);
+  /**
+   * register the {@link BaseCommand} in the new {@link CommandLine} Object
+   */
+  public static final CommandLine commandLine = new CommandLine(new BaseCommand());
 
-    private static File currentLocation = new File(".");
+  /**
+   * Logger to output useful information to the user
+   */
+  private static final Logger LOG = LoggerFactory.getLogger(SimpleCmd.class);
 
+  private static File currentLocation = new File(".");
 
-    /**
-     * Main starting point of the CLI. Here we parse the arguments from the user.
-     *
-     * @param args list of arguments the user has passed
-     */
-    public static void main(String... args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                System.out.println("Please input a line");
-                long then = System.currentTimeMillis();
-                String line = scanner.nextLine();
-                long now = System.currentTimeMillis();
-                System.out.printf("Waited %.3fs for user input%n", (now - then) / 1000d);
-                System.out.printf("User input was: %s%n", line);
+  /**
+   * Main starting point of the CLI. Here we parse the arguments from the user.
+   *
+   * @param args list of arguments the user has passed
+   */
+  public static void main(String... args) {
 
-                if (line.equals("exit")) {
-                    break;
-                }
+    try (Scanner scanner = new Scanner(System.in)) {
+      while (true) {
+        System.out.println("Please input a line");
+        long then = System.currentTimeMillis();
+        String line = scanner.nextLine();
+        long now = System.currentTimeMillis();
+        System.out.printf("Waited %.3fs for user input%n", (now - then) / 1000d);
+        System.out.printf("User input was: %s%n", line);
 
-                String[] arguments = line.split(" ");
-
-                LOG.debug("Current working directory: {}", System.getProperty("user.dir"));
-
-                if (commandLine.execute(arguments) == 0) {
-                    LOG.debug("Commands were executed correctly");
-                }
-            }
-        } catch (IllegalStateException | NoSuchElementException e) {
-            // System.in has been closed
-            System.out.println("System.in was closed; exiting");
+        if (line.equals("exit")) {
+          break;
         }
-    }
 
-    public static File getCurrentLocation() {
-        return currentLocation;
-    }
+        String[] arguments = line.split(" ");
 
-    public static void setCurrentLocation(File currentLocation) {
-        SimpleCmd.currentLocation = currentLocation;
+        LOG.debug("Current working directory: {}", System.getProperty("user.dir"));
+
+        if (commandLine.execute(arguments) == 0) {
+          LOG.debug("Commands were executed correctly");
+        }
+      }
+    } catch (IllegalStateException | NoSuchElementException e) {
+      // System.in has been closed
+      System.out.println("System.in was closed; exiting");
     }
+  }
+
+  /**
+   * @return {@link File} pointing to current location, to be used across application
+   */
+  public static File getCurrentLocation() {
+
+    return currentLocation;
+  }
+
+  /**
+   * Sets current Location to provided {@link File}, can be used across application
+   *
+   * @param currentLocation {@link File}
+   */
+  public static void setCurrentLocation(File currentLocation) {
+
+    SimpleCmd.currentLocation = currentLocation;
+  }
 }
