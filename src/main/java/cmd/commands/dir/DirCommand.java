2486148bfbfdcefe13b65430effdb62f10ee45db
SaLud2023 <sabrina.ludwig@capgemini.com>
2023-08-08 10:51:04 +0200

    Feature #3: Expand dir command

--- src/main/java/cmd/commands/dir/DirCommand.java
+++ src/main/java/cmd/commands/dir/DirCommand.java
@@ -1,76 +1,91 @@
 package cmd.commands.dir;
 
-import cmd.SimpleCmd;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import picocli.CommandLine.Command;
-
 import java.io.File;
 import java.util.Comparator;
 import java.util.stream.Stream;
 
-import static picocli.CommandLine.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cmd.SimpleCmd;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 /**
  * "List Directory" command class
  * <p/>
  * Executing the command lists all the files and folders in the current working directory.
  *
- * @see SimpleCmd#getCurrentLocation() 
- * @see SimpleCmd#setCurrentLocation(File) 
+ * @see SimpleCmd#getCurrentLocation()
+ * @see SimpleCmd#setCurrentLocation(File)
  */
-@Command(
-        name = "dir",
-        description = "Displays files and directories of current working directory",
-        mixinStandardHelpOptions = true)
+@Command(name = "dir", description = "Displays files and directories of current working directory", mixinStandardHelpOptions = true)
 public class DirCommand implements Runnable {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DirCommand.class);
-    @Option(names = {"-f", "--files"}, description = "print only file names, directories will not be listed")
-    private boolean filesOnly;
-    @Option(names = {"-s", "--sort"}, description = "possible values are {asc, desc} for ascending / descending order")
-    private String sortOrder;
+  private static final Logger LOG = LoggerFactory.getLogger(DirCommand.class);
 
+  @Option(names = { "-f", "--files" }, description = "print only file names, directories will not be listed")
+  private boolean filesOnly;
 
-    public DirCommand() {
-        /* intentionally empty */
+  @Option(names = { "-s", "--sort" }, description = "possible values are {asc, desc} for ascending / descending order")
+  private String sortOrder;
+
+  @Parameters(index = "0", description = "path of the directory", arity = "0..1")
+  private File file;
+
+  /**
+   * The constructor.
+   */
+  public DirCommand() {
+
+    /* intentionally empty */
+  }
+
+  @Override
+  public void run() {
+
+    if (this.file == null) {
+      listFilesInDirectory(SimpleCmd.getCurrentLocation());
+    } else {
+      listFilesInDirectory(this.file);
     }
+  }
 
-    @Override
-    public void run() {
-        listFilesInDirectory(SimpleCmd.getCurrentLocation());
-    }
+  private void listFilesInDirectory(File directory) {
 
-    private void listFilesInDirectory(File directory) {
-        if (directory.isDirectory()) {
-            File[] files = directory.listFiles();
-            if (null != files) {
-                Stream.of(files).sorted(getFileListComparator()).forEach(this::printLine);
-            }
-        }
+    if (directory.isDirectory()) {
+      File[] files = directory.listFiles();
+      if (null != files) {
+        Stream.of(files).sorted(getFileListComparator()).forEach(this::printLine);
+      }
     }
+  }
 
-    private Comparator<String> getSortOrderAwareFileNameComparator(final String sortOrder) {
-        if ("desc".equals(sortOrder)) {
-            return String::compareTo;
-        } else if ("asc".equals(sortOrder)) {
-            return Comparator.reverseOrder();
-        } else {
-            return (fileName1, fileName2) -> 0;
-        }
-    }
+  private Comparator<String> getSortOrderAwareFileNameComparator(final String sortOrder) {
 
-    private Comparator<File> getFileListComparator() {
-        return Comparator.comparing(File::getName, getSortOrderAwareFileNameComparator(sortOrder));
+    if ("desc".equals(sortOrder)) {
+      return String::compareTo;
+    } else if ("asc".equals(sortOrder)) {
+      return Comparator.reverseOrder();
+    } else {
+      return (fileName1, fileName2) -> 0;
     }
+  }
 
-    private void printLine(File f) {
-        if (filesOnly) {
-            if (!f.isDirectory()) {
-                LOG.info("{}\n", f.getAbsolutePath());
-            }
-        } else {
-            LOG.info("{}\n", f.getAbsolutePath());
-        }
+  private Comparator<File> getFileListComparator() {
+
+    return Comparator.comparing(File::getName, getSortOrderAwareFileNameComparator(this.sortOrder));
+  }
+
+  private void printLine(File f) {
+
+    if (this.filesOnly) {
+      if (!f.isDirectory()) {
+        LOG.info("{}\n", f.getAbsolutePath());
+      }
+    } else {
+      LOG.info("{}\n", f.getAbsolutePath());
     }
+  }
 }
